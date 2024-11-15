b127e8bca5058287d78a024477f5c9b04fa8841b
moaoh <k010103@naver.com>
2024-11-16 02:39:01 +0900

    feat: CustomDll.cpp 구현완료

--- custom_dll/CustomDll.cpp
+++ custom_dll/CustomDll.cpp
@@ -6,9 +6,38 @@ dst : 출력영상
 kernelSize : 블러링 커널 크기 ( 블러의 강도 )
 */
 bool CustomDll::ImageBlur(const ImageObject* src, ImageObject* dst, const int kernelSize) {
-    std::cout << "CustomDll::ImageBlur" << std::endl;
-    /*
-        직접구현
-     */
+
+    int imageWidth = src->getWidth();
+    int imageHeight = src->getHeight();
+
+    std::vector<unsigned char> inputBuffer = src->getBuffer();
+    std::vector<unsigned char> outputBuffer(imageWidth * imageHeight * src->getChannels());
+
+    int kernelRadius = kernelSize / 2;
+
+    for (int pixelIndex = 0; pixelIndex < inputBuffer.size(); pixelIndex++) {
+        int centerY = pixelIndex / imageWidth;
+        int centerX = pixelIndex % imageWidth;
+        int pixelSum = 0;
+        for (int offsetY = -kernelRadius; offsetY <= kernelRadius; offsetY++) {
+            for (int offsetX = -kernelRadius; offsetX <= kernelRadius; offsetX++) {
+                int neighborY = centerY + offsetY;
+                int neighborX = centerX + offsetX;
+
+                if (0 <= neighborY && neighborY < imageHeight && 0 <= neighborX && neighborX < imageWidth) {
+                    int neighborIndex = neighborY * imageWidth + neighborX;
+                    pixelSum += inputBuffer[neighborIndex];
+                } else {
+                    pixelSum += inputBuffer[pixelIndex];
+                }
+            }
+        }
+        outputBuffer[pixelIndex] = pixelSum / (kernelSize * kernelSize);
+    }
+    dst->setBuffer(outputBuffer);
+    dst->setWidth(imageWidth);
+    dst->setHeight(imageHeight);
+    dst->setChannels(src->getChannels());
+
     return true;
 }
