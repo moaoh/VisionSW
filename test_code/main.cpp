ac8c365f39be9df2432aafa3774703e17409045c
moaoh <k010103@naver.com>
2024-11-18 03:07:02 +0900

    feat: main문 형식 변경
    추후 refactoring 필요

--- test_code/main.cpp
+++ test_code/main.cpp
@@ -1,18 +1,41 @@
-#include <iostream>
-#include <fstream>
-
-#include <opencv2/opencv.hpp>
-
 #include "Processor.hpp"
 #include "OpencvDll.hpp"
 #include "CustomDll.hpp"
+#include "Logger.hpp"
+
+void compareAndLog(const std::string& imagePath, const cv::Mat& mat1, const cv::Mat& mat2) {
+  if (mat1.size() != mat2.size() || mat1.type() != mat2.type() || cv::countNonZero(mat1 != mat2) != 0) {
+    Logger::instance().info(imagePath + " are different");
+  }
+  else {
+    Logger::instance().info(imagePath + " are identical");
+  }
+}
 
 int main(int argc, const char * argv[]) {
-    OpencvDll opencv("opencv_");
-    CustomDll custom("custom_");
+  OpencvDll opencv("opencv");
+  CustomDll custom("custom");
 
+  try {
     GetConfigProcessor config(argc, argv);
-    Processor(config, opencv);
-    Processor(config, custom);
-    return 0;
+    Processor opencvProcessor(opencv);
+    Processor customProcessor(custom);
+
+    for (int i = 0; i < config.getImagePathsSize(); i++) {
+      std::string outputPath = config.getOutputPath();
+      int kernelSize = config.getKernelSize();
+      std::string imagePath = config.getImagePaths()[i];
+
+      cv::Mat customMat = customProcessor.run(imagePath, outputPath, kernelSize);
+      cv::Mat opencvMat = opencvProcessor.run(imagePath, outputPath, kernelSize);
+      if (!opencvMat.empty() && !customMat.empty()) {
+        compareAndLog(imagePath, opencvMat, customMat);
+      }
+    }
+  }
+  catch(const std::exception& e) {
+    std::cerr << "[ERROR] " << e.what() << std::endl;
+  }
+
+  return 0;
 }
